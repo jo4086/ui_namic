b293638e231e18c1326e807cbf54ee85e4e33c8b
jo4086 <ghzktm5892@gmail.com>
2025-01-31 01:21:58 +0900

    update

--- src/keys/cssKey.js
+++ src/keys/cssKey.js
@@ -1,12 +1,17 @@
-const flexPropsKeys = [
+export const pseudoElementKeys = ['before', 'after', 'first-line', 'first-letter', 'placeholder', 'selection', 'marker']
+
+// flex 속성
+export const flexPropsKeys = [
     'display', // 반드시 'flex'여야 유효
     'flexDirection',
     'justifyContent',
     'alignItems',
     'gap',
 ]
+
+
 // 그리드 박스 관련 속성
-const gridPropsKeys = [
+export const gridPropsKeys = [
     'display', // 반드시 'grid'여야 유효
     'gridTemplateColumns',
     'gridTemplateRows',
@@ -15,20 +20,12 @@ const gridPropsKeys = [
     'justifyContent',
 ]
 
-const tablePropsKeys = [
-    'colspan', // 테이블 속성
-    'rowspan',
-    'cellSpacing',
-    'cellPadding',
-    'borderCollapse',
-    'captionSide',
-    'emptyCells',
-    'tableLayout',
-]
+// 테이블 속성
+export const tablePropsKeys = ['colspan', 'rowspan', 'cellSpacing', 'cellPadding', 'borderCollapse', 'captionSide', 'emptyCells', 'tableLayout']
 
 // 공통 속성
-const commonPropsKeys = [
-    'padding', // 공통속성
+export const commonPropsKeys = [
+    'padding',
     'paddingLeft',
     'paddingRight',
     'paddingTop',
@@ -83,37 +80,5 @@ const commonPropsKeys = [
     'transition',
     'transform',
     'whiteSpace',
-    'animation'
+    'animation',
 ]
-
-const validKeys = {
-    flex: flexPropsKeys,
-    grid: gridPropsKeys,
-    table: tablePropsKeys,
-}
-
-const allCssKeys = new Set([...commonPropsKeys, ...tablePropsKeys, ...gridPropsKeys, ...flexPropsKeys])
-
-const styleCssKeys = (config) => {
-    const { stringProps, attribute } = config
-
-    const validCssKeys = new Set([...commonPropsKeys, ...(attribute ? validKeys[attribute] || [] : [])])
-
-    const validCss = {}
-    const invalidCss = {}
-    const strings = {}
-
-    Object.entries(stringProps).forEach(([key, value]) => {
-        if (validCssKeys.has(key)) {
-            validCss[key] = value
-        } else if (allCssKeys.has(key)) {
-            invalidCss[key] = value
-        } else {
-            strings[key] = value
-        }
-    })
-
-    return { validCss, strings }
-}
-
-export default styleCssKeys
